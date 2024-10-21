4bbc8723c9b204c927833b1b2f6c2b5c8e3a802e
Shrikrishna2000 <shrikrishnasoni11+code@gmail.com>
2024-10-21 22:30:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/house-price')
 def project():
     return render_template('projects.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
